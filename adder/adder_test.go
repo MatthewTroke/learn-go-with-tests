1a6ae2d9b2071049af7f0ff47c2b802ac0a5a6c3
matthewtroke <trokematthew@gmail.com>
2021-12-13 10:09:22 -0800

    Finished "Pointers & errors" section

--- adder/adder_test.go
+++ adder/adder_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestAdder(t *testing.T) {
+func TestAdd(t *testing.T) {
 	sum := Add(2, 2)
 	expected := 4
 
@@ -14,7 +14,7 @@ func TestAdder(t *testing.T) {
 	}
 }
 
-func ExampleAdder() {
+func ExampleAdd() {
 	sum := Add(1, 5)
 	fmt.Println(sum)
 	// Output: 6
---
+++ pointers-and-errors/wallet.go
@@ -0,0 +1,40 @@
+package wallet
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Stringer interface {
+	String() string
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+
+	return nil
+}
